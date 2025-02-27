17dd340084d827cdafa0cd5b27481adee59e52c8
victortsrodrigues <victadeu@gmail.com>
2025-02-27 15:07:03 -0300

    first commit

---
+++ src/test/java/com/feirinha/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feirinha.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
